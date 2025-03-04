e7424d6ef5bc4e98a9b4f458d2856561cbf24f1f
Gustav Toft <guto@factbird.com>
2025-03-04 10:55:46 +0100

    First commit with cookiecutter temp

---
+++ src/master/train.py
@@ -0,0 +1,10 @@
+from master.model import Model
+from master.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
